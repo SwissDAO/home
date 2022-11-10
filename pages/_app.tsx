7c5d8dfcba3a44dccc23ecaccfeabd4628ce25cf
Olivier <olivier.winkler@icloud.com>
2022-11-10 08:52:59 +0100

    feat(contract): add basic handling of membercard

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,60 @@
+import {
+  connectorsForWallets, getDefaultWallets, RainbowKitProvider
+} from '@rainbow-me/rainbowkit';
+import '@rainbow-me/rainbowkit/styles.css';
+import {
+  argentWallet, ledgerWallet, trustWallet
+} from '@rainbow-me/rainbowkit/wallets';
 import type { AppProps } from 'next/app';
+import { chain, configureChains, createClient, WagmiConfig } from 'wagmi';
+import { infuraProvider } from 'wagmi/providers/infura';
+import { publicProvider } from 'wagmi/providers/public';
 import '../styles/globals.scss';
+
+const { chains, provider } = configureChains(
+  [
+    //chain.mainnet,
+    chain.goerli
+  ],
+  [
+    infuraProvider({ apiKey: process.env.INFURA_KEY! }),
+    publicProvider(),
+  ]
+);
+
+const { wallets } = getDefaultWallets({
+  appName: 'swissDAO Membership Card',
+  chains,
+});
+
+const appInfo = {
+  appName: 'swissDAO Membership Card',
+};
+
+const connectors = connectorsForWallets([
+  ...wallets,
+  {
+    groupName: 'Apps',
+    wallets: [
+      argentWallet({ chains }),
+      trustWallet({ chains }),
+      ledgerWallet({ chains }),
+    ],
+  },
+]);
+
+const wagmiClient = createClient({
+  autoConnect: true,
+  connectors,
+  provider,
+});
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <Component {...pageProps} />
-  )
+    <WagmiConfig client={wagmiClient}>
+      <RainbowKitProvider appInfo={appInfo} chains={chains}>
+        <Component {...pageProps} />
+      </RainbowKitProvider>
+    </WagmiConfig>
+  );
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,28 +1,110 @@
+import { ConnectButton } from '@rainbow-me/rainbowkit';
 import Head from 'next/head';
 import styled from 'styled-components';
+import Spline from "@splinetool/react-spline";
+import { useAccount, useBalance, useContractRead } from 'wagmi';
+import Image from 'next/image';
+import { useEffect, useState } from 'react';
+import MEMBERCARD_ABI from '../artifacts/Membercard.json';
+import { ethers } from 'ethers';
 
 const Container = styled.div`
   min-height: 100vh;
-  padding: 4rem 0;
+  padding: 8em 2em;
   flex: 1;
-  display: flex;
-  flex-direction: column;
-  justify-content: space-evenly;
-  align-items: center;
 `;
 
-export default function Home() {
-  return (
-    <Container>
-      <Head>
-        <title>🇨🇭swissDAO🏔️ - Membership Card</title>
-        <meta name="description" content="swissDAO Membership Card" />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+const Header = styled.div`
+  position: absolute;
+  top: 0;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  border-bottom: 1px solid white;
+  padding: 0 2em;
+  width: 100%;
+  height: 100px;
+`
 
-      <h1>
-        🇨🇭swissDAO🏔️ Membership Card:
-      </h1>
-    </Container>
+const Grid = styled.div`
+  display: grid;
+  justify-content: center;
+  align-items: center;
+  height: 100%;
+  grid-template-columns: 50% 50%;
+`
+
+const GridItem = styled.div``
+
+export default function Home() {
+  const { isConnected, address } = useAccount();
+
+  const [account, setAccount] = useState(false);
+  const [balance, setBalance] = useState(0)
+
+  const { data: balanceOf } = useContractRead({
+    address: '0x4772b8ce690cEC47c22f6B2FE1cFD6F3E6Db29ad',
+    abi: MEMBERCARD_ABI,
+    functionName: 'balanceOf',
+    args: [address],
+  });
+
+  // https://github.com/vercel/next.js/discussions/35773 -> 🙂 smh
+  useEffect(() => {
+    setAccount(isConnected);
+    setBalance(Number(ethers.utils.formatUnits(ethers.BigNumber.from(balanceOf || 0), 0)));
+  }, [balanceOf, isConnected])
+
+  return (
+    <>
+      <Header>
+        <h1>
+          🇨🇭swissDAO🏔️ Membership Card
+        </h1>
+        <ConnectButton />
+      </Header>
+
+      <Container>
+        <Head>
+          <title>🇨🇭swissDAO🏔️ - Membership Card</title>
+          <meta name="description" content="swissDAO Membership Card" />
+          <link rel="icon" href="/favicon.ico" />
+        </Head>
+
+        <Grid>
+          <GridItem>
+            {!account && (
+              <h2>Your Keys to the Ecosystem!</h2>
+            )}
+
+            {account && balance <= 0 && (
+              <>
+                <h2>No Membership Card found!</h2>
+
+                <h3>Mint a Membership Card here:</h3>
+
+                <button>Mint</button>
+              </>
+            )}
+
+            {account && balance > 0 && (
+              <>
+                <h2>Your Membership Card:</h2>
+
+                <h3>Skills</h3>
+                <ul>
+                  <li>Solidity: 100</li>
+                  <li>DAO: 50</li>
+                </ul>
+              </>
+            )}
+
+          </GridItem>
+          <GridItem>
+            <Image height={400} width={400} src="/videos/SwissDAO_Sneakpeak.gif" alt="" />
+          </GridItem>
+        </Grid>
+      </Container>
+    </>
   )
 }
