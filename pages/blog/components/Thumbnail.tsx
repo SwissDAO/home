b81bd3fe48253769d7936e0c024d5a5a522061c3
Yves Boutellier <63062257+yvesbou@users.noreply.github.com>
2023-01-21 14:44:18 +0100

    thumbnail finished

--- pages/blog/components/Thumbnail.tsx
+++ pages/blog/components/Thumbnail.tsx
@@ -1,9 +1,43 @@
-interface IThumbnailProps {
+import { Container, Card, Text, Row, User } from "@nextui-org/react";
+import { Article } from "../../../models/article";
+import Image from "next/image";
 
+interface IThumbnailProps {
+    blog: Article
 }
 
 const Thumbnail = (props: IThumbnailProps) => {
-    return <div></div>
+    const { blog } = props;
+
+    // get height of image via cms
+    
+    return (
+        <Card css={{ mw: "600px", "margin-top": "2rem", "border-radius": "0.35rem", "padding": "2rem" }}>
+            <Card.Body>
+            <Card.Header>
+                <Container fluid css={{ "height": "300px", "objectFit": 'cover', "position": "relative"}}>
+                    <Image src={blog.coverImage} fill alt={blog.title}></Image>
+                </Container>
+            </Card.Header>
+            <Card.Body>
+                <Text h3>{blog.title}</Text>
+                <Row>
+                <Text h5 css={{"padding-right": "0.75rem"}}>{blog.category}</Text>
+                <Text h5>{blog.date}</Text>
+                </Row>
+                <Text>
+                {blog.abstract}
+                </Text>
+            </Card.Body>
+            <User
+                css={{"padding-top": "1rem"}}
+                src="https://i.pravatar.cc/150?u=a042581f4e29026704d"
+                name={blog.author}
+                description="DAO Core"
+            />
+            </Card.Body>
+        </Card>
+    )
 }
 
 export default Thumbnail;
--- pages/blog/index.tsx
+++ pages/blog/index.tsx
@@ -3,6 +3,7 @@ import { Article } from "../../models/article";
 
 import { Container, Row, Col, Card, Text, Grid, Spacer } from '@nextui-org/react';
 import { User } from "@nextui-org/react";
+import Thumbnail from "./components/Thumbnail";
 
 interface IBlogProps {}
 
@@ -14,29 +15,8 @@ const Blog = (props: IBlogProps) => {
         {
           blogarticles.slice(0,2).map((blog: Article)=> {
             return(
-              <Card css={{ mw: "600px", "margin-top": "2rem", "border-radius": "0.35rem", "padding": "2rem" }}>
-                <Card.Body>
-                  {/* <Card.Header>
-                  image
-                  </Card.Header> */}
-                  <Card.Body>
-                    <Text h3>{blog.title}</Text>
-                    <Row>
-                      <Text h5 css={{"padding-right": "0.75rem"}}>{blog.category}</Text>
-                      <Text h5>{blog.date}</Text>
-                    </Row>
-                    <Text>
-                      {blog.abstract}
-                    </Text>
-                  </Card.Body>
-                  <User
-                    css={{"padding-top": "1rem"}}
-                    src="https://i.pravatar.cc/150?u=a042581f4e29026704d"
-                    name={blog.author}
-                    description="DAO Core"
-                  />
-                </Card.Body>
-              </Card>)
+              <Thumbnail blog={blog}></Thumbnail>
+              )
             })
         }
       </Container>
