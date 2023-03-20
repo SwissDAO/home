6d268b25f99010fbe14b92cf7e3db44189fe370b
Olivier <olivier.winkler@icloud.com>
2023-03-20 21:17:20 +0100

    feat(index): use scss module styling

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,37 +1,23 @@
-import { Container, Text } from '@nextui-org/react';
+import styles from '../styles/module/home.module.scss';
 
 export default function Home() {
   return (
-    <Container
-      display="flex"
-      direction="column"
-      justify="center"
-      alignContent="center"
-      css={{
-        height: '100vh',
-        textAlign: 'center',
-      }}
-    >
-      <Text h1 css={{ margin: 0 }}>
+    <div className={styles.container}>
+      <h1>
         The First DAO <br />
         for
-      </Text>
-      <Text
-        h1
-        css={{
-          textGradient: '90deg, #E31D1C, #FF0080',
-        }}
-      >
+      </h1>
+      <h1 className={styles.gradient}>
         Switzerland.³
-      </Text>
+      </h1>
 
-      <Text h3>
+      <h3>
         ³ web3 spaces w/ Meetups, Workshops and Hackerhouses in real life
-      </Text>
-      <Text h3>³ Community spirit: Connect, Learn and Build</Text>
-      <Text h3>³ Just do Web3: Take Ownership. LFG 🚀</Text>
+      </h3>
+      <h3>³ Community spirit: Connect, Learn and Build</h3>
+      <h3>³ Just do Web3: Take Ownership. LFG 🚀</h3>
 
-      <Text h5>
+      <h5>
         Grab a ☕️ and read our{' '}
         <a
           target="_blank"
@@ -41,31 +27,7 @@ export default function Home() {
           📖
         </a>
         .
-      </Text>
-
-      {/* <Stack
-              direction={'column'}
-              spacing={1}
-              align={'center'}
-              alignSelf={'center'}>
-            <Button mt="1" colorScheme='red' size='md'>
-              <NextLink href="https://forms.gle/qvDhsqWsJiT9fAVi6" passHref>
-                <Link target="_blank" rel="noopener noreferrer">Newsletter</Link>
-              </NextLink>
-            </Button>
-          </Stack> */}
-      {/* <Stack
-            direction={'column'}
-            spacing={3}
-            align={'center'}
-            alignSelf={'center'}
-            position={'relative'}>
-            <Button mt="1" leftIcon={<SiDiscord />} color='#5865F2' variant='outline' colorScheme='whiteAlpha' size='md'>
-              <NextLink href="https://discord.com/invite/5kvQ3Q6G4x" passHref>
-                <Link target="_blank" rel="noopener noreferrer">Join Discord</Link>
-              </NextLink>
-            </Button>
-          </Stack> */}
-    </Container>
+      </h5>
+    </div>
   );
 }
