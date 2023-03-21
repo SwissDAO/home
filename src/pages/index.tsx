2beac3ac68a7c17538825b5b02969784ea451538
Olivier <olivier.winkler@icloud.com>
2023-03-21 21:07:44 +0100

    feat(grid): improve grid implementation

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,7 @@
 import GradientLine from '../components/gradient-line/gradient-line';
 import Section from '../components/section/section';
 import styles from '../styles/module/home.module.scss';
+import Image from 'next/image'
 
 export default function Home() {
   return (
@@ -24,21 +25,45 @@ export default function Home() {
             <GradientLine />
           </div>
 
-          <p>h1</p>
-          <h2>
+          <Image
+            className={styles.item}
+            src="/images/illustration_find.svg"
+            alt="Illustration Build"
+            width={400}
+            height={300}
+          />
+          <h2 className={styles.item}>
             <span className={styles.accent}>³ Find</span> peers to share your learning and building experience.
           </h2>
 
-          <h2>
+          <h2 className={styles.item}>
             <span className={styles.accent}>³ Learn</span> with our workshops to accelerate your web3 journey.
           </h2>
-          <p>h1</p>
+          <Image
+            className={styles.item}
+            src="/images/illustration_learn.svg"
+            alt="Illustration Build"
+            width={300}
+            height={300}
+          />
 
-          <p>h1</p>
-          <h2>
+          <Image
+            className={styles.item}
+            src="/images/illustration_build.svg"
+            alt="Illustration Build"
+            width={300}
+            height={300}
+          />
+          <h2 className={styles.item}>
             <span className={styles.accent}>³ Build</span> with us a DAO that is going to be the home of web3 builders.
           </h2>
         </div>
+      </Section>
+
+      <Section>
+        <h2>
+          <span className={styles.accent}>DAOtize</span> the world with us!
+        </h2>
       </Section>
     </>
   );
