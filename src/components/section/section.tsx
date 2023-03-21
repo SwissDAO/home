2e839af0524ed7487acae4a7644a7a28ade4d5c0
Olivier <olivier.winkler@icloud.com>
2023-03-21 20:40:33 +0100

    feat(section): add section component

---
+++ src/components/section/section.tsx
@@ -0,0 +1,15 @@
+import { ReactNode } from "react";
+import styles from './section.module.scss';
+
+type Props = {
+  children: ReactNode
+};
+
+export default function Section({ children }: Props) {
+
+  return (
+    <section className={styles.section}>
+      {children}
+    </section>
+  );
+};
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,21 +1,26 @@
 import GradientLine from '../components/gradient-line/gradient-line';
+import Section from '../components/section/section';
 import styles from '../styles/module/home.module.scss';
 
 export default function Home() {
   return (
     <div className={styles.container}>
-      <h1>
-        The Swiss Web3 Builder
-      </h1>
-      <h1 className={styles.gradient}>
-        Community.³
-      </h1>
+      <Section>
+        <h1>
+          The Swiss Web3 Builder
+        </h1>
+        <h1 className={styles.gradient}>
+          Community.³
+        </h1>
 
-      <p className="lead">
-        <span className={styles.gradient}>³</span> Our goal is to connect builders with projects and join forces to build the new internet.
-      </p>
+        <p className="lead">
+          <span className={styles.gradient}>³</span> Our goal is to connect builders with projects and join forces to build the new internet.
+        </p>
+      </Section>
 
-      <GradientLine />
+      <Section>
+        <GradientLine />
+      </Section>
     </div>
   );
 }
