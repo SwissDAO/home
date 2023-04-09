89e9e499d94630d1a87ca6527bc8ed8d1ab556a4
Olivier <olivier.winkler@icloud.com>
2023-04-09 12:59:08 +0200

    feat(navbar): custom navbar with gradient line

--- src/components/navbar/navbar.tsx
+++ src/components/navbar/navbar.tsx
@@ -1,12 +1,9 @@
-import { Navbar as NextUiNavbar } from '@nextui-org/react';
-import { ConnectButton } from '@rainbow-me/rainbowkit';
 import Link from 'next/link';
-import { useAccount } from 'wagmi';
 import Logo from '../logo/logo';
+import styles from './navbar.module.scss';
+import GradientLine from '../gradient-line/gradient-line';
 
 const Navbar = () => {
-  const { isConnected } = useAccount();
-
   const navItems = [
     {
       // label: isConnected ? 'Your Membership' : 'Membership',
@@ -34,42 +31,19 @@ const Navbar = () => {
   );
 
   return (
-    <NextUiNavbar isBordered variant={'static'}>
-      <NextUiNavbar.Brand
-        css={{
-          '@xs': {
-            w: '12%',
-          },
-        }}
-      >
+    <>
+      <nav className={styles.navbar}>
         <Link href="/">
           <Logo width={128} height={128} />
         </Link>
-      </NextUiNavbar.Brand>
-      <NextUiNavbar.Content hideIn="xs">
-        {navItems.map((item, i) => renderNavItem(item, i))}
 
-        {/* {process.env.NODE_ENV === 'development' && (
-          <NextUiNavbar.Item>
-            <ConnectButton />
-          </NextUiNavbar.Item>
-        )} */}
-      </NextUiNavbar.Content>
-      <NextUiNavbar.Toggle aria-label="toggle navigation" showIn="xs" />
-      <NextUiNavbar.Collapse>
-        {navItems.map((item, i) => (
-          <NextUiNavbar.CollapseItem key={i}>
-            {renderNavItem(item, i)}
-          </NextUiNavbar.CollapseItem>
-        ))}
+        <div className={styles.items}>
+          {navItems.map((item, i) => renderNavItem(item, i))}
+        </div>
 
-        {/* {process.env.NODE_ENV === 'development' && (
-          <NextUiNavbar.CollapseItem>
-            <ConnectButton />
-          </NextUiNavbar.CollapseItem>
-        )} */}
-      </NextUiNavbar.Collapse>
-    </NextUiNavbar>
+      </nav>
+      <GradientLine horizontal />
+    </>
   );
 };
 
