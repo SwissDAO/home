cb65dc85327c3a66c99282b35e8e531398c23148
Olivier <olivier.winkler@icloud.com>
2023-04-11 20:11:18 +0200

    feat(teaser): add teaser component

---
+++ src/pages/teaser.tsx
@@ -0,0 +1,10 @@
+import Section from "../components/section/section";
+import styles from '../styles/module/teaser.module.scss';
+
+export default function Teaser() {
+  return (
+    <Section>
+      <h1 className={styles.animatedGradient}>Coming soon!</h1>
+    </Section>
+  )
+};
