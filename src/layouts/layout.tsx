1a34b8e1d167e3f76352b10b4b90484a736c396a
Olivier <olivier.winkler@icloud.com>
2023-04-11 15:44:57 +0200

    feat(seo): improve seo

--- src/layouts/layout.tsx
+++ src/layouts/layout.tsx
@@ -4,11 +4,30 @@ import Footer from '../components/footer/footer';
 import Navbar from '../components/navbar/navbar';
 
 export default function Layout({ children }: { children: ReactNode }) {
+
+  const title = 'swissDAO - The Swiss Web3 Builder Community.³';
+  const description = 'Our goal is to connect builders with projects and join forces to build the new internet.';
+  const keywords = 'swissDAO, swissdao, daosuisse, suissedao, web3, switzerland, builder, community, decentralized finance, blockchain,';
+  const ogImage = 'https://swissdao.space/favicon.ico';
+
   return (
     <>
       <Head>
-        <title>swissDAO - The First DAO for Switzerland.³</title>
-        <meta property="og:title" content="swissDAO" key="title" />
+        <title>{title}</title>
+        <meta property="og:title" content={title} key="title" />
+        <meta name="twitter:title" content={title} />
+
+        <meta name="description" content={description} />
+        <meta name="og:description" content={description} />
+        <meta name="twitter:description" content={description} />
+
+        {keywords && <meta name="keywords" content={keywords} />}
+
+        <meta property="og:image" content={ogImage} />
+        <meta name="twitter:image" content={ogImage} />
+
+        <meta name="copyright" content="swissDAO" />
+        <meta name="twitter:card" content="summary_large_image" />
       </Head>
       <Navbar />
       <main>{children}</main>
